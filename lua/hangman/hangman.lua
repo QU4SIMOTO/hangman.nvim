d426e10e04462146a535a468494e0fd784e113e5
QU4SIMOTO <jack@ifrit.localdomain>
2024-12-21 22:52:13 +0000

    Add test for random word when new game

---
+++ lua/hangman/hangman.lua
@@ -0,0 +1,37 @@
+local Ui = require("hangman.ui")
+local Game = require("hangman.game")
+
+local Hangman = {}
+Hangman.__index = Hangman
+
+function Hangman:new()
+  return setmetatable({
+    game = Game:new(),
+    ui = Ui:new({
+      relative = "editor",
+      width = 27,
+      height = 15,
+      border = "rounded",
+      style = "minimal",
+      zindex = 100,
+      title = "Hangman",
+      title_pos = "center",
+    }),
+  }, self)
+end
+
+function Hangman:toggle_ui()
+  self.ui:toggle(self.game)
+end
+
+function Hangman:guess(c)
+  self.game:guess(c)
+end
+
+function Hangman:new_game()
+  self.game = Game:new()
+  self.ui:toggle(self.game)
+  self.ui:toggle(self.game)
+end
+
+return Hangman
---
+++ tests/test_hangman.lua
@@ -0,0 +1,25 @@
+local new_set = MiniTest.new_set
+local Hangman = require("hangman.hangman")
+
+local T = new_set({})
+
+T["new game random word"] = function()
+  -- TODO test random word generation better than this
+  local hangman = Hangman:new()
+  local word = Hangman:new().game.word
+  local tries = 50
+  for _ = 1, tries do
+    hangman:new_game()
+    if word ~= hangman.game.word then
+      return
+    end
+  end
+  error(
+    "Didn't get a new word after "
+    .. tries ..
+    ", word: "
+    .. word
+  )
+end
+
+return T
