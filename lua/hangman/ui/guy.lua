d8130daacc21719927b8179d528ea4ff348ee3ca
QU4SIMOTO <jack@ifrit.localdomain>
2024-12-22 13:17:30 +0000

    Use util to pad and center letter line

--- lua/hangman/ui/guy.lua
+++ lua/hangman/ui/guy.lua
@@ -1,5 +1,4 @@
----@type integer The width of the ascii hangman image
-local image_cols = 6
+local util = require("hangman.util")
 
 ---@type string[][] Ascii images of the hangman guy
 local images = {
@@ -96,7 +95,6 @@ local images = {
 ---@class HangmanGuy UI component displaying the hangman guy
 ---@field win integer?
 ---@field buf integer?
----@field offset integer?
 ---@field settings HangmanGuySettings
 local HangmanGuy = {}
 HangmanGuy.__index = HangmanGuy
@@ -107,7 +105,6 @@ function HangmanGuy:new(settings)
   return setmetatable({
     win = nil,
     buf = nil,
-    offset = 0,
     settings = settings,
   }, self)
 end
@@ -120,7 +117,7 @@ function HangmanGuy:render(game)
   local lines = vim
     .iter(images[image_id])
     :map(function(line)
-      return string.rep(" ", self.offset) .. line
+      return util.pad_and_center(line, self.settings.win.width)
     end)
     :totable()
   vim.api.nvim_buf_set_lines(self.buf, 0, 1, false, lines)
@@ -134,7 +131,6 @@ end
 function HangmanGuy:create_window(game, col, row)
   self.buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_set_option_value("filetype", "hangman", { buf = self.buf })
-  self.offset = math.floor(self.settings.win.width / 2) - (image_cols / 2)
   self:render(game)
   local window_config =
     vim.tbl_extend("force", self.settings.win, { col = col, row = row })
